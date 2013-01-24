4d24f2cf537aaa35ab73e57176a8c795d96f6b2f
masaki <bcmh1220224@gn.iwasaki.ac.jp>
2013-01-24 10:26:07 +0900

    応用プログラミングA の課題1

---
+++ CL_K01_2/CL_K01_2.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+/*作成者：1T4(12)_服部 勝輝*/
+/*課題名 CL_K01_2*/
+
+void main (void){
+	int width;
+	width = 5;													/*一辺の長さ*/
+	printf("一辺の%dの長方形の面積は%dです。",width,width*width);	/*面積を計算し出力*/
+
+}
